ed54dd4fbbe8914f537ea040eaf9dca1b20ce54e
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-12 08:59:26 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,21 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+ 
+ if board[index] == "X" || "O"
+   return true
+ end
+ 
+ if board[index] == " " 
+   return false
+  end
+  
+  if board[index] == ""
+    return false
+  end
+  
+  if board[index] == nil
+    return false
+  end
+  
+end
